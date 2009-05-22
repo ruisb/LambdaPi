706e8d3ea5ea786c32501ceb33dc7aa49e71f475
Vali <vali@me.com>
2009-05-22 13:22:01 +0200

    refactoring again

--- Interpreter/Types.hs
+++ Interpreter/Types.hs
@@ -1,29 +1,26 @@
 module Interpreter.Types where
 
-  data Name
-     =  Global  String
-     |  Local   Int  String
-     |  Quote   Int  String  --should not be needed
-    deriving (Show {--, Eq--})
+data Name
+   =  Global  String
+   |  Local   Int  String
+   |  Quote   Int  String  --should not be needed
+  deriving (Show {--, Eq--})
 
-  instance Eq Name where
-    (Global s1) == (Global s2) = s1 == s2
-    (Local n1 _) == (Local n2 _) = n1==n2
-    (Quote n1 _) == (Quote n2 _) = n1 == n2
-    _ == _ = False
+instance Eq Name where
+  (Global s1) == (Global s2) = s1 == s2
+  (Local n1 _) == (Local n2 _) = n1==n2
+  (Quote n1 _) == (Quote n2 _) = n1 == n2
+  _ == _ = False
 
+type Result a = Either String a
 
+type NameEnv v = [(Name, v)]
+type Ctx inf = [(Name, inf)]
+type State v inf = (Bool, String, NameEnv v, Ctx inf)
 
-
-  type Result a = Either String a
-
-  type NameEnv v = [(Name, v)]
-  type Ctx inf = [(Name, inf)]
-  type State v inf = (Bool, String, NameEnv v, Ctx inf)
-
-  data Stmt i tinf = Let String i           --  let x = t
-                   | Assume [(String,tinf)] --  assume x :: t, assume x :: *
-                   | Eval i
-                   | PutStrLn String        --  lhs2TeX hacking, allow to print "magic" string
-                   | Out String             --  more lhs2TeX hacking, allow to print to files
-    deriving (Show)
+data Stmt i tinf = Let String i           --  let x = t
+                 | Assume [(String,tinf)] --  assume x :: t, assume x :: *
+                 | Eval i
+                 | PutStrLn String        --  lhs2TeX hacking, allow to print "magic" string
+                 | Out String             --  more lhs2TeX hacking, allow to print to files
+  deriving (Show)
